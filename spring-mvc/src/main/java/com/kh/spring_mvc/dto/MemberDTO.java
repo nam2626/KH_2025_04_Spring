10fe91a611d140cd2f3c1b7b048ef6e3b90ed664
nam2626 <nam2626@naver.com>
2025-07-22 13:04:00 +0900

    롬복 적용

--- spring-mvc/src/main/java/com/kh/spring_mvc/dto/MemberDTO.java
+++ spring-mvc/src/main/java/com/kh/spring_mvc/dto/MemberDTO.java
@@ -1,60 +1,67 @@
 package com.kh.spring_mvc.dto;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class MemberDTO {
     private String id;
     private String password;
     private String name;
     private int age;
 
-    public MemberDTO(String id, String password, String name, int age) {
-        this.id = id;
-        this.password = password;
-        this.name = name;
-        this.age = age;
-    }
-
-    public MemberDTO() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "MemberDTO{" +
-                "id='" + id + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
+//    public MemberDTO(String id, String password, String name, int age) {
+//        this.id = id;
+//        this.password = password;
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public MemberDTO() {
+//    }
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "MemberDTO{" +
+//                "id='" + id + '\'' +
+//                ", password='" + password + '\'' +
+//                ", name='" + name + '\'' +
+//                ", age=" + age +
+//                '}';
+//    }
 }
