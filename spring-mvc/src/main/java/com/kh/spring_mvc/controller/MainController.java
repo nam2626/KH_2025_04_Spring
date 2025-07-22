98daa9215ac2c122e148b9e7e80aafe30da04b7f
nam2626 <nam2626@naver.com>
2025-07-22 09:49:43 +0900

    파라미터 확인

--- spring-mvc/src/main/java/com/kh/spring_mvc/controller/MainController.java
+++ spring-mvc/src/main/java/com/kh/spring_mvc/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.kh.spring_mvc.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -21,4 +23,22 @@ public class MainController {
         System.out.println("로그인 페이지 요청됨");
         return "login";// forward 방식으로 이동
     }
+
+    @PostMapping("/member/login")
+    public String login(HttpServletRequest request){
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        System.out.println(username + " : " + password);
+        return "main";
+    }
 }
+
+
+
+
+
+
+
+
+
