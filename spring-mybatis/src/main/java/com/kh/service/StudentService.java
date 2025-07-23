75b6596c032cb1dbc566f8657f583e8f9bbaaffe
nam2626 <nam2626@naver.com>
2025-07-23 16:52:32 +0900

    mapper 작업 전

--- spring-mybatis/src/main/java/com/kh/service/StudentService.java
+++ spring-mybatis/src/main/java/com/kh/service/StudentService.java
@@ -1,7 +1,20 @@
 package com.kh.service;
 
+import com.kh.dto.StudentDTO;
+import com.kh.mapper.StudentMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
+    private StudentMapper studentMapper;
+
+    public StudentService(StudentMapper studentMapper) {
+        this.studentMapper = studentMapper;
+    }
+
+    public List<StudentDTO> selectAllStudent() {
+        return studentMapper.selectAllStudent();
+    }
 }
