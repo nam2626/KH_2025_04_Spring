e5a4e135d3a6107add8067032055543dbbdae408
nam2626 <nam2626@naver.com>
2025-07-24 16:18:09 +0900

    학과정보 삭제 처리 컨트롤러까지 완료

--- spring-mybatis/src/main/java/com/kh/controller/MajorController.java
+++ spring-mybatis/src/main/java/com/kh/controller/MajorController.java
@@ -1,23 +1,37 @@
 package com.kh.controller;
 
+import com.kh.dto.MajorDTO;
+import com.kh.service.MajorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RequestMapping("/major")
 @RestController
 public class MajorController {
-
+  @Autowired
+  private MajorService majorService;
   //삭제
   @DeleteMapping("/{mno}")
   public Map<String, Object> deleteMajor(@PathVariable String mno){
-    System.out.println("삭제할 학과번호 : " + mno);
     Map<String, Object> map = new HashMap<>();
-    map.put("msg","삭제 호출 성공");
+    System.out.println("삭제할 학과번호 : " + mno);
+    //실제 삭제 작업 수행
+    int count = majorService.deleteMajor(mno);
+    if(count == 0)
+      map.put("msg","학과 정보 삭제 실패");
+    else
+      map.put("msg","학과 정보 삭제 성공");
+    map.put("result",count);
+    //전체 학과정보 조회해서 map에 넣어서 전달
+    List<MajorDTO> majorList = majorService.selectAllMajor();
+    map.put("majorList", majorList);
     return map;
   }
 
