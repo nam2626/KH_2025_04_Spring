744e82cf8ea651fb776baa8e5615cf7cb8154009
nam2626 <nam2626@naver.com>
2025-07-24 11:00:35 +0900

    학과리스트를 맵으로 변경

--- spring-mybatis/src/main/java/com/kh/controller/MainController.java
+++ spring-mybatis/src/main/java/com/kh/controller/MainController.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MainController {
@@ -27,8 +29,16 @@ public class MainController {
         List<StudentDTO> studentList = studentService.selectAllStudent();
         System.out.println(studentList);
         List<MajorDTO> majorList = majorService.selectAllMajor();
+        //학과 리스트를 맵으로 변환
+//        Map<String, String> majorMap = new HashMap<>();
+//        for (MajorDTO major : majorList) {
+//            majorMap.put(major.getMno(), major.getMname());
+//        }
+        Map<String, String> majorMap = majorList.stream()
+                .collect(HashMap::new, (map, major) -> map.put(major.getMno(), major.getMname()), HashMap::putAll);
         view.addObject("studentList", studentList);
         view.addObject("majorList", majorList);
+        view.addObject("majorMap", majorMap);
         view.setViewName("main");
         return view;
     }
