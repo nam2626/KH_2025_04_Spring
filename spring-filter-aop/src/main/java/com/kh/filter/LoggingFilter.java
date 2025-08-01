cc01a27467239213c73d8c932dabb69885f82185
nam2626 <nam2626@naver.com>
2025-08-01 10:44:26 +0900

    요청 처리시간 확인

--- spring-filter-aop/src/main/java/com/kh/filter/LoggingFilter.java
+++ spring-filter-aop/src/main/java/com/kh/filter/LoggingFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 import java.io.IOException;
 
@@ -22,11 +23,18 @@ public class LoggingFilter implements Filter {
   @Override
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
+    StopWatch stopWatch = new StopWatch();
+
     HttpServletRequest request = (HttpServletRequest) servletRequest;
     log.info("Request received : {}",request.getRequestURI());
 
+    stopWatch.start();
     //필터 작업 수행 후 서블릿으로 요청을 전달, 이 코드가 없으면 요청이 중단됨
     filterChain.doFilter(servletRequest,servletResponse);
+    stopWatch.stop();
+
+    long totalTime = stopWatch.getTotalTimeMillis();
+    log.info("요청 처리까지 완료 시간 : {}ms",totalTime);
   }
 
   @Override
