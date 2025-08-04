3027c4708b0f247e3ed33fcb9f79af39170948aa
nam2626 <nam2626@naver.com>
2025-08-04 10:54:46 +0900

    인증 관련 DTO 작성

---
+++ spring-board/src/main/java/com/kh/dto/AuthRequest.java
@@ -0,0 +1,14 @@
+package com.kh.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class AuthRequest {
+  @NotBlank(message = "사용자 아이디는 비워둘 수 없습니다.")
+  private String userid;
+  @NotBlank(message = "비밀번호는 비워둘 수 없습니다.")
+  private String password;
+  
+  private String username;//선택사항
+}
