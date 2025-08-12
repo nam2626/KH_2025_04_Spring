5c3094f346357552f9632269352145067a9d3010
nam2626 <nam2626@naver.com>
2025-08-12 12:10:49 +0900

    약국 정보 조회 xml 정보 파싱

--- APITest/src/DurgStoreTest2.java
+++ APITest/src/DurgStoreTest2.java
@@ -1,21 +1,17 @@
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-public class DurgStoreTest1 {
+public class DurgStoreTest2 {
   public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
     //건강보험심사평가원_약국정보서비스 데이터 요청해서 출력
     String apiURL = "http://apis.data.go.kr/B551182/pharmacyInfoService/getParmacyBasisList?";
@@ -34,19 +30,10 @@ public class DurgStoreTest1 {
     System.out.println("ResponseCode : " + conn.getResponseCode());
     if (conn.getResponseCode() != 200)
       return;
-    try (BufferedReader br = new BufferedReader(
-            new InputStreamReader(conn.getInputStream()))) {
-      String result = "";
-      while (true) {
-        String temp = br.readLine();
-        if (temp == null) break;
-        result += temp;
-      }
-      System.out.println(result);
 
       DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newDefaultInstance();
       DocumentBuilder builder = builderFactory.newDocumentBuilder();
-      Document document = builder.parse(new InputSource(new StringReader(result)));
+      Document document = builder.parse(conn.getInputStream());
       document.getDocumentElement().normalize();
 
       NodeList list = document.getElementsByTagName("item");
@@ -64,4 +51,4 @@ public class DurgStoreTest1 {
       conn.disconnect();
     }
   }
-}
+
